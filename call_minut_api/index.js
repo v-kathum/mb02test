6cd4820761a50a2aa9937c670c1a8fda9ea74ec7
Femi Adesina <femi@firestartermedia.com>
2018-10-12 13:33:51 +0100

    --wip-- [skip ci]

--- call_minut_api/index.js
+++ call_minut_api/index.js
@@ -3,7 +3,7 @@ const _ = require('lodash')
 // const async = require('async')
 
 const minutUrl = 'https://api.minut.com'
-// const paths = ['temperature', 'sound', 'humidity', 'battery']
+// const paths = ['temperature', 'humidity', 'sound', 'battery']
 
 const getLogin = () =>
   new Promise((resolve, reject) => {
@@ -42,43 +42,6 @@ const getDeviceList = (token) =>
     })
   })
 
-// const getDeviceInformationOld = (token, devices, path) =>
-//   new Promise((resolve, reject) => {
-//     console.log('Get to Device Info Function')
-//     const devInfo = _.map(devices, (device) => {
-//       request.get(`${minutUrl}/devices/${device.id}/${path}`, {
-//         'auth': {
-//           'bearer': token
-//         },
-//         'json': true
-//       }, (error, response, body) => {
-//         if (error) {
-//           return reject(error)
-//         }
-
-//         resolve(devInfo)
-//       })
-//     })
-//   })
-
-// const getDeviceInformation = (token, devices, path) => {
-//   const names = ['temperature', 'sound', 'humidity', 'battery'].reduce((result, type) => {
-//     devices.forEach((device) => result.push({
-//       deviceId: device.device_id, type
-//     }))
-//   }, [])
-
-//   async.parallel(names, (error, result) => {
-//     console.log(result)
-//     if (error) {
-//       console.log(error)
-//     } else {
-//       console.log('Results: ', result)
-//       request.get(`${minutUrl}/draft1/admin/devices/${result[0].deviceId}/${result[0].type}`)
-//     }
-//   })
-// }
-
 const getInfo = (token, device, path) => {
   return new Promise((resolve, reject) => {
     const {
@@ -100,24 +63,32 @@ const getInfo = (token, device, path) => {
   })
 }
 
-const getDeviceInformation = async (token, device) => {
-  // const deviceInfo = []
-  // async.parallel([getInfo(token, device, 'temperature'), getInfo(token, device, 'humidity'), getInfo(token, device, 'sound'), getInfo(token, device, 'battery')], (error, results) => {
-  //   console.log('Error: ', error)
-  //   if (error) {
-  //     console.log(error)
-  //   }
-  //   console.log('Results: ', results)
-  // })
-  // deviceInfo.map(paths.forEach((path) => getInfo(token, device, path)))
-  // console.log('Device Info: ', deviceInfo)
-  return {
-    temp: await getInfo(token, device, 'temperature'),
-    humid: await getInfo(token, device, 'humidity'),
-    sound: await getInfo(token, device, 'sound'),
-    battery: await getInfo(token, device, 'battery')
-  }
-}
+const getDeviceInformation = (token, device) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const [
+        temp,
+        humid,
+        sound,
+        battery
+      ] = await Promise.all([
+        getInfo(token, device, 'temperature'),
+        getInfo(token, device, 'humidity'),
+        getInfo(token, device, 'sound'),
+        getInfo(token, device, 'battery')
+      ])
+
+      resolve({
+        init: device,
+        temp,
+        humidity,
+        sound,
+        battery
+      })
+    } catch (error) {
+      reject(error)
+    }
+  })
 
 const handler = async ({ done }) => {
   try {
@@ -126,10 +97,7 @@ const handler = async ({ done }) => {
     } = await getLogin()
 
     const devices = await getDeviceList(accessToken)
-    const device = devices[0]
-
-    const devicesInfo = await getDeviceInformation(accessToken, device)
-    await console.log('Devices Info: ', devicesInfo)
+    const deviceData = await Promise.all(devices.map((device) => getDeviceInformation(accessToken, device)))
 
     done(null, devices)
   } catch (error) {
