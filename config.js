f284673a18e982c0abaea587e79c6887cf10f009
Ahmed Nuaman <ahmed@ahmednuaman.com>
2018-11-07 19:40:23 +0000

    --wip-- [skip ci]

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports.azureSubId = process.env.azureSubId || 'b2a30fc9-e788-4a8c-8790-d684018c5151'
-module.exports.azureServicePrincipalTenantId = process.env.azureServicePrincipalTenantId || 'd2c867c9-341b-4c48-8db4-2d053805f6f8'
-module.exports.azureServicePrincipalClientId = process.env.azureServicePrincipalClientId || '632ac275-22ee-45fa-ab17-31187daaa3c4'
-module.exports.azureServicePrincipalPassword = process.env.azureServicePrincipalPassword || 'bc9e9605-a307-4a88-b8f6-2e323eb87ee9'
-// module.exports.AzureWebJobStorage = process.env.AzureWebJobStorage || ''
-module.exports.AzureQueuesConnection = process.env.AzureQueuesConnection || 'DefaultEndpointsProtocol=https;AccountName=minutstorage;AccountKey=7OUkLqCqVBfg6gPrDT/ISeQpkcUDoHRQdmsSA44jm95nsfswCnS1V8dl+PyZJ5vKKLtSW3iERBTg8PpEe/dt1Q==;'
-module.exports.AzureDBConnection = process.env.AzureDBConnection || 'AccountEndpoint=https://minut-cosmosdb.documents.azure.com:443/;AccountKey=tXh5uY6uun4Go3MDE5NkqXwbLJA64sxo56qiA32zoFaPlWZDFMJ95EUxe5uiK26oVx1Mn6rWwgCbJzaQ0wTPpA==;'
-
-// module.exports.azureServicePrincipalPassword = process.env.azureServicePrincipalPassword || 'bc9e9605-a307-4a88-b8f6-2e323eb87ee9'
--- src/server/db/constant.js
+++ src/server/db/constant.js
@@ -1,4 +1,4 @@
-module.exports.DB_COLLECTION_NAME = process.env.DB_COLLECTION_NAME || 'device'
-module.exports.DB_HOST = process.env.DB_HOST || 'https://minut-cosmosdb.documents.azure.com:443/'
-module.exports.DB_NAME = process.env.DB_NAME || 'Devices'
-module.exports.DB_KEY = process.env.DB_KEY || 'tXh5uY6uun4Go3MDE5NkqXwbLJA64sxo56qiA32zoFaPlWZDFMJ95EUxe5uiK26oVx1Mn6rWwgCbJzaQ0wTPpA=='
+module.exports.DB_COLLECTION_NAME = process.env.DB_COLLECTION_NAME || 'devices'
+module.exports.DB_HOST = process.env.DB_HOST || 'https://minut-db.documents.azure.com:443/'
+module.exports.DB_NAME = process.env.DB_NAME || 'devices'
+module.exports.DB_KEY = process.env.DB_KEY || 'LeVtIh77Qhe7BysE3yyje1V2r9wN3hvsuorqcdMHYCoILxIra1WdpoB54Drw5nx2H0DebuL0637BETWpP9Bvig=='
