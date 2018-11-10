6f9bfe7ef4125c72ad0d29e7e87504677ba452e7
Ahmed Nuaman <ahmed@ahmednuaman.com>
2018-11-10 21:59:04 +0000

    --wip-- [skip ci]

---
+++ hook/post-deploy.sh
@@ -0,0 +1,1 @@
+az functionapp config appsettings set -n minut-rhp -g minut-rhp-rg --settings FUNCTIONS_EXTENSION_VERSION=2.0.11961-alpha minut-db_DOCUMENTDB="AccountEndpoint=https://minut-db.documents.azure.com:443/;AccountKey=LeVtIh77Qhe7BysE3yyje1V2r9wN3hvsuorqcdMHYCoILxIra1WdpoB54Drw5nx2H0DebuL0637BETWpP9Bvig==;"
