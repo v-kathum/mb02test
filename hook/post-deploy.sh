fb64ce06feea56bb9c2122570976e7921f6ed211
Ahmed Nuaman <ahmed@ahmednuaman.com>
2018-11-18 08:52:41 +0000

    --wip-- [skip ci]

--- hook/post-deploy.sh
+++ hook/post-deploy.sh
@@ -1,7 +1,8 @@
-az functionapp config appsettings set -n minut-rhp -g minut-rhp-rg --settings \
-FUNCTIONS_EXTENSION_VERSION=~2 \
+az functionapp config appsettings set -n wandle-function-v2 -g wandle-function-v2-rg --settings \
+FUNCTIONS_EXTENSION_VERSION="~2" \
 minut-db_DOCUMENTDB="AccountEndpoint=https://minut-db.documents.azure.com:443/;AccountKey=LeVtIh77Qhe7BysE3yyje1V2r9wN3hvsuorqcdMHYCoILxIra1WdpoB54Drw5nx2H0DebuL0637BETWpP9Bvig==;" \
 minut-queue-cosmosdb="DefaultEndpointsProtocol=https;AccountName=xwndxp5fkg55aazfunctions;AccountKey=qstDUZJzjrp2kM8nBcoImcA1t56QbE7o/OSgW6EHV4bRM5gV52CbUgWjSyNh7AhghUY8D+hwYvy6GSBc7AeiJw==;EndpointSuffix=core.windows.net" \
-minut-queue-postgresdb="DefaultEndpointsProtocol=https;AccountName=xwndxp5fkg55aazfunctions;AccountKey=qstDUZJzjrp2kM8nBcoImcA1t56QbE7o/OSgW6EHV4bRM5gV52CbUgWjSyNh7AhghUY8D+hwYvy6GSBc7AeiJw==;EndpointSuffix=core.windows.net"
+minut-queue-postgresdb="DefaultEndpointsProtocol=https;AccountName=xwndxp5fkg55aazfunctions;AccountKey=qstDUZJzjrp2kM8nBcoImcA1t56QbE7o/OSgW6EHV4bRM5gV52CbUgWjSyNh7AhghUY8D+hwYvy6GSBc7AeiJw==;EndpointSuffix=core.windows.net" \
+FUNCTIONS_WORKER_RUNTIME="node"
 
-az functionapp restart -n minut-rhp -g minut-rhp-rg
+az functionapp restart -n wandle-function-v2 -g wandle-function-v2-rg
